ce213fce45d8a56c9ae10f2a16211c30640c8baa
nicoleychen <66758558+nicoleychen@users.noreply.github.com>
2020-06-10 21:24:49 -0400

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,12 @@
+function four(){
+  window.open("four.html");
+}
+function three(){
+  window.open("three.html");
+}
+function two(){
+  window.open("two.html");
+}
+function one(){
+  window.open("one.html");
+}
